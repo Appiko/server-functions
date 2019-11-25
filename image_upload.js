8708d1cfedd717b08d055dc966acc52f23fd985b
Prathik <pratik862pj@live.com>
2019-11-25 17:58:53 +0530

    Custom file handling

--- image_upload.js
+++ image_upload.js
@@ -7,33 +7,29 @@ const PORT = 3100
 const app = require('express')()
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+var stringify = require('json-stringify-safe');
+const fs = require('fs');
 
 app.use(helmet())
 app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: 'image/png', limit: '50mb' }))
 
 const FCM = require('fcm-node')
-const serverKey = require('./sense-ele-firebase-service-account.json')
-const fcm = new FCM(serverKey)
-
-const multer = require('multer')
-const storage = multer.diskStorage(
-    {
-        destination: './uploads/',
-        filename: function (req, file, cb) {
-            console.log("GOT " + file.originalname);
-            const extension = file.originalname.split('.').pop();
-            cb(null, `${file.originalname.split('.')}-${Date.now()}.${extension}`);
-        }
-    }
-);
-const upload = multer({ storage: storage });
+// const serverKey = require('./sense-ele-firebase-service-account.json')
+// const fcm = new FCM(serverKey)
 
 
 app.listen(PORT, () => {
     console.log(`${APP_NAME} listening on port ${PORT}...`)
 })
-app.get('/image', upload.single('image'), function (req, res, next) {
 
+app.get('/image', function (req, res, next) {
+    res.send(`Get working`);
+});
+
+app.post('/image/:filename', function (req, res) {
+
+    saveImage(`${req.params.filename}_${Date.now()}_${req.params.filename.split('.').pop()}`, req.body);
 
     const message = {
         to: '/topics/image',
@@ -52,5 +48,19 @@ app.get('/image', upload.single('image'), function (req, res, next) {
             console.log(`Sent alert for image`)
         }
     })
-    res.send(`Sent alert for image`);
+
 });
+
+function saveImage(filename, data) {
+    var myBuffer = new Buffer.alloc(data.length);
+    for (var i = 0; i < data.length; i++) {
+        myBuffer[i] = data[i];
+    }
+    fs.writeFile('./uploads/r/' + filename, myBuffer, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+}
