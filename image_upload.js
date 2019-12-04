0bccfd263996f2eed3e08326ad655875433df3e6
Prathik <pratik862pj@live.com>
2019-12-04 12:43:39 +0530

    :sparkle: final image upload

--- image_upload.js
+++ image_upload.js
@@ -1,44 +1,30 @@
-const APP_NAME = "Image upload"
-const PORT = 3600
+const PORT = 3600;
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const serveIndex = require('serve-index');
+const fs = require('fs');
+const path = require('path');
 
-
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const path = require('path')
-var stringify = require('json-stringify-safe')
-const fs = require('fs')
-const serveIndex = require('serve-index')
-
-app.use(helmet())
-app.use(bodyParser.json())
-app.use(bodyParser.raw({ type: 'image/png', limit: '50mb' }))
-
-
-
-app.use('/uploads', serveIndex((path.join(__dirname, 'uploads'))));
-app.use('/uploads', express.static((path.join(__dirname, 'uploads'))));
 const FCM = require('fcm-node')
 const serverKey = require('./sense-ele-firebase-service-account.json')
 const fcm = new FCM(serverKey)
 
+const app = express();
 
-app.listen(PORT, () => {
-    console.log(`${APP_NAME} listening on port ${PORT}...`)
-})
+app.use(bodyParser.raw({ type: "*/*" }));
 
-app.get('/image', function (req, res, next) {
-    res.send(`Get working`);
-});
+app.use('/uploads', serveIndex((path.join(__dirname, 'uploads'))));
+app.use('/uploads', express.static((path.join(__dirname, 'uploads'))));
 
-app.post('/image/:filename', function (req, res) {
+app.listen(PORT, console.log(PORT));
 
-    fileName = `${req.params.filename}_${Date.now()}.${req.params.filename.split('.').pop()}`
+app.post('/chunked/:filename', function (req, res) {
+
+    fileName = `/chunked/${req.params.filename}_${Date.now()}.${req.params.filename.split('.').pop()}`
     saveImage(fileName, req.body);
-
     res.sendStatus(200);
+
 
     const message = {
         to: `/topics/image`,
@@ -62,15 +48,15 @@ app.post('/image/:filename', function (req, res) {
             console.log(`Sent alert for new image`)
         }
     })
-});
 
+});
 
 function saveImage(filename, data) {
     var myBuffer = new Buffer.alloc(data.length);
     for (var i = 0; i < data.length; i++) {
         myBuffer[i] = data[i];
     }
-    fs.writeFile('./uploads/r/' + filename, myBuffer, function (err) {
+    fs.writeFile(`${__dirname}/uploads/r/` + filename, myBuffer, function (err) {
         if (err) {
             console.log(err);
         } else {
