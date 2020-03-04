31241ed43fbd6beae07b27d1ab5c4fd5b03fbe63
Prathik <pratik862pj@live.com>
2020-03-04 17:14:04 +0530

    Folder Structre for server functions
    Branched into 3 folders
    1. prod - scripts in production
    2. test - scripts running on the test server
    3. archive - scripts not currently in use

--- prod/gateway_rx.ts
+++ prod/gateway_rx.ts
@@ -13,10 +13,17 @@ interface Array<T> {
     spliceFirst(): number;
 }
 
+interface Number {
+    toRadians(): number;
+}
+
 Array.prototype.spliceFirst = function () {
     return this.splice(0, 1)[0]
 }
 
+Number.prototype.toRadians = function () {
+    return this.valueOf() * (Math.PI / 180);
+}
 
 app.use(helmet())
 app.use(bodyParser.raw())
@@ -29,7 +36,7 @@ app.listen(PORT, () => {
 
 
 
-app.post('/', (req: any, res: any) => {
+app.post('/', async (req: any, res: any) => {
     let z = [...req.body]
 
     if (z.length >= 5) {
@@ -40,12 +47,22 @@ app.post('/', (req: any, res: any) => {
 
         let result: boolean = true;
         while (z.length !== 0 && result) {
-            let packetType: number = z.spliceFirst()
-            let payloadLength: number = z.spliceFirst()
-            let payload: Array<number> = z.splice(0, payloadLength)
+            let packetType: number = 0;
+            let payloadLength: number = 0;
+            let payload: Array<number> = [];
+            if (z.length > 4) {
+                packetType = z.spliceFirst()
+                payloadLength = z.spliceFirst()
+                payload = z.splice(0, payloadLength)
+                console.log(`type: ${packetType}, length: ${payloadLength}, payload: ${payload}`)
+            } if (z.length == 3) {
+                packetType = z.spliceFirst()
+                payloadLength = 2
+                payload = z
+                console.log(`type: ${packetType}, length: ${payloadLength}, payload: ${payload}`)
+            }
 
-            console.log(`type: ${packetType}, length: ${payloadLength}, payload: ${payload}`)
-            result = parsePacket(gateway_dep_id, gateway_dev_id, packetType, payload);
+            result = await parsePacket(gateway_dep_id, gateway_dev_id, packetType, payload);
         }
         if (result) {
             res.status(200).send();
@@ -62,7 +79,7 @@ app.post('/', (req: any, res: any) => {
 function parsePacket(gateway_dep_id: number, gateway_dev_id: number, packetType: number, payload: Array<number>) {
 
     switch (packetType) {
-        case 0:
+        case 2:
             return parseGatewayPacket(gateway_dep_id, gateway_dev_id, payload)
         case 1:
             return parseRFPacket(gateway_dep_id, gateway_dev_id, payload)
@@ -79,55 +96,115 @@ function parseGatewayPacket(gateway_dep_id: number, gateway_dev_id: number, payl
         return false;
     }
 
-    let vBattery: number = payload.spliceFirst() * 0.025;
-    let vSupply: number = payload.spliceFirst() * 0.025;
+    let vBattery: number = payload.spliceFirst() * (3.6 / 255);
+    let vSupply: number = payload.spliceFirst() * (3.6 / 255);
 
     sendGatewayPacket(gateway_dep_id, gateway_dev_id, vBattery, vSupply);
     return true;
 }
 
 
-function parseRFPacket(gateway_dep_id: number, gateway_dev_id: number, payload: Array<number>) {
+async function parseRFPacket(gateway_dep_id: number, gateway_dev_id: number, payload: Array<number>) {
     console.log("RF PKT")
     if (payload.length < 5) {
         return false;
     }
+    let rssi = payload.spliceFirst()
     let deploymentId = payload.spliceFirst()
     let deviceId = getDeviceId(payload)
     let nodePacketType = payload.spliceFirst()
-    let nodeVBattery = payload.spliceFirst() * 0.025
+    let nodeVBattery = payload.spliceFirst() * (3.6 / 255)
 
     console.log(`${deploymentId}, ${deviceId}, ${nodePacketType}, ${nodeVBattery}`);
-    switch (nodePacketType) {
-        case 0:
-            console.log("ALIVE")
-            sayToNode(deploymentId, deviceId, nodeVBattery, 'alive');
-            return true;
-            break;
+    switch (deploymentId) {
         case 1:
-            console.log("SENSING")
-            sendAlert(gateway_dep_id, gateway_dev_id, deploymentId, deviceId);
+            let payloadToSave = [...payload];
+            payloadToSave.push(rssi);
+            // get lat lon
+            let nodeLat = getNextDeg(payload);
+            let nodeLon = getNextDeg(payload);
+
+            let [init_lat, init_lon] = await getLatLon(deploymentId, deviceId);
+            let diffMts = getLatLonDiff(nodeLat, nodeLon, init_lat, init_lon);
+
+            //check if alert
+            console.log(`Diff meters = ${diffMts}`);
+            if (diffMts > 10) {
+                sendAlert(gateway_dep_id, gateway_dev_id, deploymentId, deviceId);
+            }
+
+            // update
             sayToNode(deploymentId, deviceId, nodeVBattery, 'alive');
-            saveData(gateway_dep_id, gateway_dev_id, deploymentId, deviceId, payload);
-            return true;
+            saveData(gateway_dep_id, gateway_dev_id, deploymentId, deviceId, payloadToSave);
+
             break;
         case 2:
-            console.log("MAINTENANCE")
-            sayToNode(deploymentId, deviceId, nodeVBattery, 'maintenance');
-            saveData(gateway_dep_id, gateway_dev_id, deploymentId, deviceId, payload);
-            return true;
+            switch (nodePacketType) {
+                case 0:
+                    console.log("ALIVE")
+                    sayToNode(deploymentId, deviceId, nodeVBattery, 'alive');
+                    return true;
+                    break;
+                case 1:
+                    console.log("SENSING")
+                    sendAlert(gateway_dep_id, gateway_dev_id, deploymentId, deviceId);
+                    sayToNode(deploymentId, deviceId, nodeVBattery, 'alive');
+                    saveData(gateway_dep_id, gateway_dev_id, deploymentId, deviceId, payload);
+                    return true;
+                    break;
+                case 2:
+                    console.log("MAINTENANCE")
+                    sayToNode(deploymentId, deviceId, nodeVBattery, 'maintenance');
+                    saveData(gateway_dep_id, gateway_dev_id, deploymentId, deviceId, payload);
+                    return true;
+                    break;
+                default:
+                    console.log("BAD PACKET")
+                    break;
+            }
             break;
         default:
-            console.log("BAD PACKET")
             break;
     }
-
     //TODO: Impr/remove
     console.log(`Battery voltage = ${nodeVBattery}; Data = ${payload}`)
     return false;
 }
+function getNextDeg(payload: number[]) {
+    return parseInt(payload.splice(0, 4).reverse().map(payload => payload.toString(16)).join(''), 16) / 1000000;
+}
+
+function getLatLonDiff(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    console.log([lat1, lon1, lat2, lon2]);
+    var R = 6371e3; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2 - lat1).toRadians();
+    var Δλ = (lon2 - lon1).toRadians();
+
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    var d = R * c;
+    console.log(φ1);
+    console.log(d);
+    return d;
+}
+
+async function getLatLon(node_deployment_id: number, node_device_id: number) {
+    let query = `query MyQuery5 {
+        node(where: {active: {_eq: true}, _and: {deployment_id: {_eq: ${node_deployment_id}}, _and: {device_id: {_eq: ${node_device_id}}}}}){
+          location
+        }
+      }`
+    let data = await talkToDB(query);
+    return data['node'][0]['location'].replace(')', '').replace('(', '').split(',').map((x: string) => parseFloat(x))
+}
 
 function sendGatewayPacket(gateway_dep_id: number, gateway_dev_id: number, vBattery: number, vSupply: number) {
+    console.log([gateway_dep_id, gateway_dev_id, vBattery, vSupply])
     let query: String = `mutation MyMutation {
         update_gateway(where: {deployment_id: {_eq: ${gateway_dep_id}}, _and: {device_id: {_eq: ${gateway_dev_id}}, _and: {active: {_eq: true}}}}, _set: {alive: true, battery_voltage: "${vBattery}", supply_voltage: "${vSupply}", last_updated: "now()"}) {
           affected_rows
